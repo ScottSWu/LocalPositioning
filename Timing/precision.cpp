61d642d01d34bacd456da0fadf462205d333b0aa
ScottSWu <scott.wu.hjx@gmail.com>
2016-01-01 07:59:11 +0000

    Updated timing

--- Timing/precision.cpp
+++ Timing/precision.cpp
@@ -1,3 +1,6 @@
+// Don't run while running tmux =(
+// Don't run while running anything really
+
 #include <stdio.h>
 #include <time.h>
 
@@ -9,26 +12,69 @@ int m3diff(const timespec& s, const timespec& e) {
     return (e.tv_sec - s.tv_sec) * 1000 + (e.tv_nsec - s.tv_nsec) / 1000000;
 }
 
+int m4diff(const timespec& s, const timespec& e) {
+    return (e.tv_sec - s.tv_sec) * 10000 + (e.tv_nsec - s.tv_nsec) / 100000;
+}
+
+int m5diff(const timespec& s, const timespec& e) {
+    return (e.tv_sec - s.tv_sec) * 100000 + (e.tv_nsec - s.tv_nsec) / 10000;
+}
+
+int m6diff(const timespec& s, const timespec& e) {
+    return (e.tv_sec - s.tv_sec) * 1000000 + (e.tv_nsec - s.tv_nsec) / 1000;
+}
+
 int main() {
     timespec t,n;
+    int diff;
+    const int rows = 200;
+    const int cols = 50;
+    int index = 0;
+    int result[4 * rows * cols];
+
+    index = 0;
     gettime(t);
     gettime(n);
-    int diff;
-    const int rows = 20;
-    const int cols = 20;
-    int index = 0;
-    int result[rows * cols];
     for (int i = 0, l = rows * cols; i < l; i++) {
         while ((diff = m3diff(t, n)) <= 0) gettime(n);
         result[index++] = diff;
         t = n;
     }
 
-    index = 0;
-    for (int i = 0; i < rows; i++) {
-    for (int j = 0; j < cols; j++) {
-        printf("%d ", result[index++]);
+    gettime(t);
+    gettime(n);
+    for (int i = 0, l = rows * cols; i < l; i++) {
+        while ((diff = m5diff(t, n)) <= 0) gettime(n);
+        result[index++] = diff;
+        t = n;
     }
-    printf("\n");
+
+    gettime(t);
+    gettime(n);
+    for (int i = 0, l = rows * cols; i < l; i++) {
+        while ((diff = m5diff(t, n)) <= 0) gettime(n);
+        result[index++] = diff;
+        t = n;
+    }
+
+    gettime(t);
+    gettime(n);
+    for (int i = 0, l = rows * cols; i < l; i++) {
+        while ((diff = m6diff(t, n)) <= 0) gettime(n);
+        result[index++] = diff;
+        t = n;
+    }
+
+    
+    index = 0;
+    for (int d = 3; d <= 6; d++) {
+        printf("m%d\n", d);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                printf("%d ", result[index++]);
+            }
+            printf("\n");
+        }
+        printf("\n");
     }
 }
